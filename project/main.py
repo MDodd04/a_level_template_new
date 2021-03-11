51dbd687cd350d9966970ca7cb7d8b143869d8db
MDodd04 <80475781+MDodd04@users.noreply.github.com>
2021-03-11 16:41:08 +0000

    Update main.py
    Added hello world function

--- project/main.py
+++ project/main.py
@@ -2,6 +2,9 @@
 def greeting(name):
     return f'Hello {name}!'
 
+# Hello world function
+def hello_world():
+    print("Hello World")
 
 # Run this script when this module is run directly
 if __name__ == "__main__":
